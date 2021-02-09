0a977027ecc2ce8268963488142e280538aa97ec
KA3SS <kais.amirouche@gmail.com>
2021-02-09 11:53:09 +0100

    play

--- wp-content/themes/sably/header.php
+++ wp-content/themes/sably/header.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://kit.fontawesome.com/ceee3d5b7f.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/style.css">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 
@@ -10,9 +13,16 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div class="wrap">
-<div id="page" class="site">
+	<?php wp_body_open(); ?>
+	<div class="wrap">
+		<div id="page" class="site">
 
-	<header id="masthead" class="site-header">
-	</header><!-- #masthead -->
+			<header id="header" class="site-header">
+				<div class="nav_logo">
+					<img class="logo" src="<?php echo get_template_directory_uri() ?>/assets/img/logo_jaune1.png" alt="Logo Sably">
+				</div>
+				<div class="nav_buttons">
+				</div>
+				<div class="nav_login">
+				</div>
+			</header><!-- #masthead -->
--- wp-content/themes/sably/template-home.php
+++ wp-content/themes/sably/template-home.php
@@ -5,8 +5,12 @@ Template Name: Home
 
 get_header();
 ?>
-
-<h1>Home</h1>
+<sextion id="About" class="midle">
+    <div class="petite-boite">
+        <h1>bienvenue sur notre site</h1>
+        <i class="fas fa-play-circle" ></i>
+    </div>
+</sextion>
 
 <?php
 get_footer();
