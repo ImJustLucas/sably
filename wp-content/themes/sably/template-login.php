9b3cd80c0a5106d99cfe0b4b85166d38d12c377c
ImJustLucas <contact@lucasbellier.fr>
2021-02-09 15:12:28 +0100

    work on login page

---
+++ wp-content/themes/sably/template-login.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: login
+*/
+
+get_header();
+?>
+
+<section id="intro">
+    <p>Se connecter</p>
+</section>
+
+<div class="wrap-sheet">
+    <div id="sheet">
+        <section id="login">
+            <h2 class="titleSection">Connexion</h2>
+
+            <form action="" id="fromLogin">
+            
+            <div class="input-area">
+                <label for="username-login">Votre nom d'utilisateur :</label>
+                <input type="text" name="username-login" id="username-login" placeholder="Username">
+                <span class="error-username-login"></span>
+            </div>
+
+            <div class="input-area">
+                <label for="password-login">Votre mot de passe :</label>
+                <input type="password" name="password-login" id="password-login">
+                <span class="error-username-login"></span>
+            </div>
+
+            <input class="btn-submit-login loginbutton" type="submit" name="submitted" value="Se connecter">
+            </form>
+
+            <div class="divider"></div>
+
+            <div class="buttonForSignin">
+                <p>Pas encore de compte ? Inscrivez-vous dès maintenant !</p>
+                <button class="loginbutton">S'inscrire</button>
+            </div>
+        </section>
+
+        <section id="signin"></section>
+    </div>
+</div>
+
+<?php
+get_footer();
