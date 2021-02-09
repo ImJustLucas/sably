4927e7341f0ee44602e6b0b3764d57593e72f3c2
Younes BESSA <bessa460@gmail.com>
2021-02-09 18:54:18 +0100

    added footer

--- wp-content/themes/sably/footer.php
+++ wp-content/themes/sably/footer.php
@@ -2,6 +2,7 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+	Copyright 2021 | SABLY. Tous droits réservés. <a class="rights" href=>Mentions légales</a>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 </div><!-- END WRAP-->
