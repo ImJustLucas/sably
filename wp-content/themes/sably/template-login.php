6bf125ec95d522b74b937665660b957e41658688
unknown <>
2021-02-09 17:00:51 +0100

    register en cours

--- wp-content/themes/sably/template-login.php
+++ wp-content/themes/sably/template-login.php
@@ -4,12 +4,23 @@ Template Name: login
 */
 
 // REGISTER PROCESS
-
+$errors = array();
 if (!empty($_POST['submitted-reg'])) {
-  $errors = array();
   $username = cleanXss($_POST['username-signin']);
-  $username = cleanXss($_POST['email-signin']);
+  $email = cleanXss($_POST['email-signin']);
   $password = cleanXss($_POST['password-signin']);
+  $password2 = cleanXss($_POST['password2-signin']);
+
+  $errors = validText($errors,$username,'username-signin', 3,10);
+  $errors = validMail($errors,$email,'email-signin');
+  $errors = validPass($errors,$password,'password-signin',$password2,3,10);
+  debug($errors);
+
+  if (empty($errors)) {
+
+    echo 'bravo';
+
+  }
 }
 
 get_header();
@@ -69,7 +80,13 @@ get_header();
                 <!-- MOT DE PASSE -->
                 <div class="input-area">
                     <label for="username-signin">Mot de passe</label>
-                    <input type="text" name="username-signin" id="username-signin" placeholder="Password">
+                    <input type="text" name="password-signin" id="password-signin" placeholder="Password">
+                    <span class="error-password-signin"></span>
+                </div>
+                <!-- MOT DE PASSE -->
+                <div class="input-area">
+                    <label for="username-signin">Confirmer mot de passe</label>
+                    <input type="text" name="password2-signin" id="password2-signin" placeholder="Confirmer password">
                     <span class="error-password-signin"></span>
                 </div>
                 <!-- SUBMIT -->
