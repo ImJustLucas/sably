461d16a716f7ca318b31940085ca4338da40e35d
unknown <>
2021-02-09 16:05:45 +0100

    register in work + add functions

--- wp-content/themes/sably/template-login.php
+++ wp-content/themes/sably/template-login.php
@@ -3,6 +3,15 @@
 Template Name: login
 */
 
+// REGISTER PROCESS
+
+if (!empty($_POST['submitted-reg'])) {
+  $errors = array();
+  $username = cleanXss($_POST['username-signin']);
+  $username = cleanXss($_POST['email-signin']);
+  $password = cleanXss($_POST['password-signin']);
+}
+
 get_header();
 ?>
 
@@ -16,7 +25,7 @@ get_header();
             <h2 class="titleSection">Connexion</h2>
 
             <form action="" id="fromLogin">
-            
+
             <div class="input-area">
                 <label for="username-login">Votre nom d'utilisateur :</label>
                 <input type="text" name="username-login" id="username-login" placeholder="Username">
@@ -44,13 +53,30 @@ get_header();
             <h2>S'inscrire</h2>
 
             <form id="fromSignin" action="" method="post">
-            
+
+                <!-- USERNAME -->
                 <div class="input-area">
-                    <label for="username-signin">Votre nom d'utilisateur :</label>
-                    <input type="text" name="username-signin" id="username-signin" placeholder="Username">
+                    <label for="username-signin">Nom d'utilisateur</label>
+                    <input type="text" name="username-signin" id="username-signin" placeholder="Nom d'utilisateur">
                     <span class="error-username-signin"></span>
                 </div>
-            
+                <!-- EMAIL -->
+                <div class="input-area">
+                    <label for="username-signin">Email</label>
+                    <input type="text" name="email-signin" id="email-signin" placeholder="Email">
+                    <span class="error-email-signin"></span>
+                </div>
+                <!-- MOT DE PASSE -->
+                <div class="input-area">
+                    <label for="username-signin">Mot de passe</label>
+                    <input type="text" name="username-signin" id="username-signin" placeholder="Password">
+                    <span class="error-password-signin"></span>
+                </div>
+                <!-- SUBMIT -->
+                <div class="input-area">
+                    <input type="submit" name="submitted-reg" id="submitted-reg" placeholder="S'inscrire">
+                </div>
+
             </form>
         </section>
     </div>
