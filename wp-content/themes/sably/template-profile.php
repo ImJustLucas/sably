701a2bb8677a6cdd8c013852d8df6d3ed87c4ca3
ImJustLucas <contact@lucasbellier.fr>
2021-02-10 15:45:21 +0100

    mini modif qui fait a diff

--- wp-content/themes/sably/template-profile.php
+++ wp-content/themes/sably/template-profile.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 
 <section id="intro">
-
+<h1 class="titleWebSite"><span class="txt-type" data-wait="3000" data-words='["bienvenue <?php if(!empty($current_user->display_name) && $current_user->display_name != ''){ echo $current_user->display_name ;} else { echo $current_user->user_login ;}?>", "Voici votre profil", "retrouvez votre cv plus bas !"]'></span>|</h1>
 </section>
 
 <div class="wrap-sheet">
