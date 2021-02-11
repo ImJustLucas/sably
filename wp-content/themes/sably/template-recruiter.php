f0d0537e9b8a2accbdaa021772c8722eeaec1355
ImJustLucas <contact@lucasbellier.fr>
2021-02-11 16:39:15 +0100

    prepare recruiter page

--- wp-content/themes/sably/template-recruiter.php
+++ wp-content/themes/sably/template-recruiter.php
@@ -6,7 +6,22 @@ Template Name: Home recruiter
 get_header();
 ?>
 
-<h1>Home Recruiter</h1>
+<div id="recruiterBackground">
+
+    <section id="intro">
+        <div class="petite-boite">
+            <h1 class="titleWebSite"><span class="txt-type" data-wait="3000" data-words='["bonjour <?php if(!empty($current_user->first_name) && $current_user->first_name != ''){ echo $current_user->first_name ;} else { echo $current_user->user_login ;}?> ! ", "Voici la liste des CVs", "Trouvez le bon candidat"]'></span>|</h1>
+        </div>
+        <p class="subTitleWebSite">Bienvenue sur votre espace membre</p>
+    </section>
+
+    <div class="wrap-sheet">
+        <div id="sheet">
+            <h2 class="titleSection">Espace recruteur</h2>
+
+        </div>
+    </div>
+</div>
 
 <?php
 get_footer();
