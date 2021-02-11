fe16ac55fb078c489e71f0c6c10f4b997c07542a
Younes BESSA <bessa460@gmail.com>
2021-02-11 10:30:36 +0100

    page 404 design+style

--- wp-content/themes/sably/footer.php
+++ wp-content/themes/sably/footer.php
@@ -1,13 +1,16 @@
 <?php
 ?>
-
-	<footer id="colophon" class="site-footer">
-		<p>Copyright 2021 | SABLY. Tous droits réservés. <a class="rights" href="Mentions legales">Mentions légales</a></p>
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<div class="footerwrap"></div>
+	<div class="contentfooter">
+		<p class="copyrights">Copyright 2021 | SABLY. Tous droits réservés. <a class="rights" href="Mentions legales">Mentions légales</a></p>
+	</div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 </div><!-- END WRAP-->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
