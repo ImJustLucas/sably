7497fb548fd0d3ff9c9701fc1092394a816cd04f
safia medjahed <safia.medjahed@gmail.com>
2021-02-12 11:38:58 +0100

    connexion-style

--- wp-content/themes/sably/template-login.php
+++ wp-content/themes/sably/template-login.php
@@ -3,7 +3,7 @@
 Template Name: login
 */
 
-if(is_user_logged_in()){
+if (is_user_logged_in()) {
     wp_redirect(site_url() . "/profile");
 }
 
@@ -13,7 +13,7 @@ global $wpdb;
 
 //CONNEXION :
 
-if(!empty($_POST['submitted'])){
+if (!empty($_POST['submitted'])) {
 
     $username = cleanXSS($_POST['username-login']);
     $password = cleanXSS($_POST['password-login']);
@@ -28,24 +28,21 @@ if(!empty($_POST['submitted'])){
     if (!is_wp_error($verify_user)) {
         $email = $verify_user->user_email;
         $wpdb_prefix = $wpdb->prefix;
-        $wpdb_tablename = $wpdb_prefix.'users';
+        $wpdb_tablename = $wpdb_prefix . 'users';
         $sql = "SELECT ID FROM $wpdb_tablename WHERE user_email = %s";
-        $id_user = $wpdb->get_var( $wpdb->prepare( $sql, $email ) );
-        $wpdb_tablename = $wpdb_prefix.'usermeta';
+        $id_user = $wpdb->get_var($wpdb->prepare($sql, $email));
+        $wpdb_tablename = $wpdb_prefix . 'usermeta';
         $sql = "SELECT user_id FROM $wpdb_tablename WHERE user_id = %d AND meta_key = %s AND meta_value = %s";
-        $user_valid = $wpdb->get_var($wpdb->prepare($sql, $id_user, 'valid_mail' ,'valid'));
+        $user_valid = $wpdb->get_var($wpdb->prepare($sql, $id_user, 'valid_mail', 'valid'));
 
-        if(!empty($user_valid)){
+        if (!empty($user_valid)) {
             wp_redirect(site_url() . "/profile");
-        }
-        else {
+        } else {
             $_POST['error-login'] = 'Veuillez valider votre compte';
         }
-
-
     } else {
         $_POST['error-login'] = 'Identifiant ou mot de passe invalide';
-   }
+    }
 }
 
 get_header();
@@ -54,40 +51,46 @@ get_header();
 <section id="intro">
     <p>Se connecter</p>
     <?php
-    if(!empty($_GET['id']) && $_GET['id'] == 'new'){ ?>
-    <p class="welcome">Vous venez de valider votre compte</p>
+    if (!empty($_GET['id']) && $_GET['id'] == 'new') { ?>
+        <p class="welcome">Vous venez de valider votre compte</p>
     <?php } ?>
 </section>
 
 <div class="wrap-sheet">
     <div id="sheet">
-        <section id="login">
-            <h2 class="titleSection">Connexion</h2>
+        <form id="formInfoUser" method="post" action="">
+            <section id="connexionUser">
+                <div class="columnUser">
+                    <h2 class="titleSection">Connexion</h2>
 
-            <form method="post" action="" id="formLogin">
+                    <div class="input-area-infoUser">
+                        <label for="name-infoUser">Username</label>
+                        <i class="fas fa-arrow-right" style="color: #ffc045"></i>
+                        <input type="text" name="name-infoUser" id="name-infoUser" <?php if (!empty($current_user->last_name) && $current_user->last_name != '') {
+                                                                                        echo 'value="' . $current_user->last_name . '"';
+                                                                                    } else {
+                                                                                        echo 'placeholder="Doe"';
+                                                                                    } ?>>
+                    </div>
+                    <span class="error-infoUser error-name-infoUser"></span>
 
-            <div class="input-area">
-                <label for="username-login">Votre nom d'utilisateur :</label>
-                <input type="text" name="username-login" id="username-login" placeholder="Username">
-                <span class="error-username-login"><?php if(!empty($_POST['error-login']) && $_POST['error-login'] != ''){echo $_POST['error-login'];} ?></span>
-            </div>
+                    <div class="input-area-infoUser">
+                        <label for="newPassword-infoUser">Mot de passe</label>
+                        <i class="fas fa-arrow-right" style="color: #ffc045"></i>
+                        <input type="password" name="newPassword-infoUser" id="newPassword-infoUser">
+                    </div>
+                    <span class="error-infoUser error-newPassword-infoUser"></span>
 
-            <div class="input-area">
-                <label for="password-login">Votre mot de passe :</label>
-                <input type="password" name="password-login" id="password-login">
-                <span class="error-username-login"></span>
-            </div>
+                    <div class="submitButtonInfoUser">
+                        <input class="btn-submit-userInfo loginbutton" type="submit" name="submittedInfoUser" value="Se connecter">
+                    </div>
 
-            <input class="btn-submit-login loginbutton" type="submit" name="submitted" value="Se connecter">
-            </form>
-
-            <div class="divider"></div>
-
-            <div class="buttonForSignin">
-                <p>Pas encore de compte ? Inscrivez-vous dès maintenant !</p>
-                <a href="<?php echo esc_url(home_url('signin')) ?>" class="button-signin">S'inscrire</a>
-            </div>
-        </section>
+                    <div class="buttonForSignin">
+                        <p>Pas encore de compte ? Inscrivez-vous dès maintenant ! </p>
+                        <a href="<?php echo esc_url(home_url('signin')) ?>" class="button-signin"> S'inscrire</a>
+                    </div>
+                </div>
+            </section>
     </div>
 </div>
 
